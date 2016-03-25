2294c8192e37c59ea3f45c3e418fd89ec60a7c01
Mike Winters <mikewinters@Mikes-MacBook-Air.local>
2016-03-25 15:35:00 -0400

    Added the remaining results, an update to the evaluation script, and also a statistical Analysis file for the dataset.

--- evaluation.m
+++ evaluation.m
@@ -6,3 +6,7 @@ r_140505 = genConfMat('140505', r);
 r_140511 = genConfMat('140511', r);
 % r_140521 = genConfMat('140521', r)
 % r_140522 = genConfMat('140522', r)
+
+confMatBig = r_140505.confusionMat+r_140507.confusionMat+r_140511.confusionMat ...
+    + r_140514.confusionMat + r_140515.confusionMat + ...
+    + r_140521.confusionMat + r_140522.confusionMat;
