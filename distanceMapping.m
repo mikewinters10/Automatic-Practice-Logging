2ebff01a3457af8883c22a40d52279336cada71e
Mike Winters <mikewinters@Mikes-MacBook-Air.local>
2016-03-27 15:13:05 -0400

    CleanUp for working directory

--- distanceMapping.m
+++
@@ -1,9 +0,0 @@
-function dist = distanceMapping(qPC, rPC, qSC, rSC);
-
-% Define distances independently based upon the absolute mean error.
-pCDist = mae(qPC - rPC);
-
-% The Question of feature normalization for the spectral centroid.
-sCDist = mae(qSC - rSC);
-
-dist = pCDist;
--- returnDistance.m
+++
@@ -1,3 +0,0 @@
-function dist = distanceMapping(qPC, rPC, qSC, rSC);
-
-pCdist = mae(qPC - rPC);
